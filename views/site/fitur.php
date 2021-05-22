7fca4464d86f1e859284b614919031f58639d50d
ArungRamadhan <65582917+ArungRamadhan@users.noreply.github.com>
2021-05-22 15:58:48 +0700

    update
    update

---
+++ views/site/fitur.php
@@ -0,0 +1,16 @@
+<h1>Fitur Yii</h1>
+<table class="table table-bordered">
+	<tr>
+		<td>Fast</td>
+		<td>Yii Gives you the maximum functionality by adding the least possible overhead
+		</td>
+	</tr>
+	<tr>
+		<td>Resure</td>
+		<td>sane default and built-in tool help you solid and resure code</td>
+	</tr>
+	<tr>
+		<td>Effesient</td>
+		<td>write more code in less time with simple, yet powerfull APIs and code generation</td>
+	</tr>
+</table>
