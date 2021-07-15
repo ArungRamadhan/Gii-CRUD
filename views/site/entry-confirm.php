ddd4d78b4ba82a6051000d60a1da09ffcbc19eaa
ArungRamadhan <65582917+ArungRamadhan@users.noreply.github.com>
2021-07-15 18:37:30 +0700

    update

--- views/site/entry-confirm.php
+++
@@ -1,9 +0,0 @@
-<?phpuse yi:\helpers\Html;
-?>
-<p>terima kasih sudah menginputkan pada form:</p>
-<ul>
-	<li><label>Name </label>: <?= Html::encode($model->name) ?></li>
-	<li><label>Email </label>: <?= Html::encode($model->email) ?>
-	<li><label>No.Telepon </label>: <?= Html::encode($model->notelp) ?>
-	<li><label>Alamat </label>: <?= Html::encode($model->alamat) ?>
-</ul>
